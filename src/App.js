d9fbffa78da756af916af983a64a24b921ee7263
John Bozarov <usaboz@gmail.com>
2020-01-11 18:03:39 -0700

    my country search project on react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios'; 
 import './App.css';
+import Header from './Components/Header'; 
+import Modify from './Components/ Modify'; 
+import Display from './Components/Display'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor () {
+    super (); 
+
+    this.state = {
+      countries: [],
+    }
+  }
+
+  componentDidMount () {
+    axios.get('https://restcountries.eu/rest/v2/all')
+        .then(res=> {
+          this.setState({countries: res.data})
+          console.log(res.data)
+        })
+        .catch(err=>console.log(err))
+  }
+
+  contrySearch = (inputVal, actual) => {
+    axios.get(`https://restcountries.eu/rest/v2/${actual}/${inputVal}`)
+        .then(res=>{
+          this.setState ({countries: res.data})
+        })
+        .catch(err=>console.log(err))
+  }
+
+  handleReset = () => {
+    axios.get('https://restcountries.eu/rest/v2/all')
+        .then(res=> {
+        this.setState({countries: res.data})
+        })
+        .catch(err=>console.log(err))
+  }
+
+  
+  delete = (contName, i) => {
+    console.log(contName, i)
+    let newList = this.state.countries.filter(val=> val.name!==contName)
+    this.setState ({countries: newList })
+  }
+
+
+  render () {
+    const {countries} = this.state
+    return (
+      <div className="App">
+          <Header /> 
+          <Modify contrySearch={this.contrySearch} handleReset={this.handleReset} /> 
+          <Display maps={countries} delete={this.delete} />
+      </div>
+      );
+    }
+  }
 
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'; 
+import '../App.css'; 
+
+
+
+export class Header extends Component {
+  render() {
+    return (
+      <div className='header' >
+        <h2> Welcome to Countries </h2>
+      </div>
+    )
+  }
+}
+
+export default Header
